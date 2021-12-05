255af3fbb0a275956e67f8404c46c7867b751fe0
dcBrian <ivucady@gmail.com>
2021-12-06 02:14:19 +0900

    remove dev args

--- src/main/java/com/project/homework/HomeworkApplication.java
+++ src/main/java/com/project/homework/HomeworkApplication.java
@@ -8,11 +8,7 @@ public class HomeworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkApplication.class, args);
-		args = new String[] {
-				"--imports",
-				"https://bigfastblog.com/embed-base64-encoded-images-inline-in-html",
-				"https://bigfastblog.com/embed-base64-encoded-images-inline-in-html"
-		};
+
 		new CommandLine().run(args);
 	}
 
